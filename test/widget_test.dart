1e21a79ead7871d1e64b9b8f524ea2addf623676
Pedro Francisco <pdrfrncsco@gmail.com>
2026-03-02 11:55:07 +0100

    chore: update app icons and fix test import
    - Update favicon and web app icons
    - Add iOS app icons for all required sizes
    - Update Android launcher icons
    - Fix test import from 'kwijiyaapp' to 'kwijiya'
    - Update iOS Contents.json with new icon configuration

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,11 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:kwijiyaapp/main.dart';
+import 'package:kwijiya/main.dart';
 
 void main() {
-  testWidgets('App builds and shows splash screen', (WidgetTester tester) async {
+  testWidgets('App builds and shows splash screen', (
+    WidgetTester tester,
+  ) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const ProviderScope(child: KwijiyaApp()));
 
